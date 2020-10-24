e547a86b653b85697387469044caea5d621632c4
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-24 10:03:21 -0500

    remove accidental require statements

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,6 @@
-const { UserInputError } = require("apollo-server-express");
-
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
-const { sign } = require("jsonwebtoken");
 
 const resolvers = {
     Query: {
