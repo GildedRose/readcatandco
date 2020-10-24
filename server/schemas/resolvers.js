e8295a5e65224f15a795dd022f0f7bb79b4ba98b
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-24 09:58:11 -0500

    add in typedefs and user/adduser resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,21 @@
+const { UserInputError } = require("apollo-server-express");
+
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        user: async () => {
+            return User.find().select('-__v -password');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,41 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+    }
+
+    type Product {
+        _id: ID
+        name: String
+        price: Float
+        description: String
+        image: String
+        category: Category
+    }
+
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
     type Query {
-        helloWorld: String
+        user: User
+        product(_id: ID!): Product
+        products(category: ID, name: String): [Product]
+        categories: [Category]
+    }
+
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     }
 `;
 
