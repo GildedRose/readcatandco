3cc481f2235bdc4a80b3a9f515d0daa78849e801
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-24 09:20:12 -0500

    add in base user, product, category model

---
+++ server/models/Product.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+
+const Product = model('Product', productSchema)
+
+module.exports = Product;
---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+      }
+
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
