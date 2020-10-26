941c14f3d068862ceafb89a1c3a4e0343861efcf
Anne <anne.k.plummer@gmail.com>
2020-10-25 21:14:14 -0500

    Routes created and working

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+
+export const About = () => (
+  <Container>
+    <Row>
+      <Col>
+      <div>
+          <h2>About</h2>
+          <p>Cat ipsum dolor sit amet, chew foot get suspicious of own shadow then go play with toilette paper. Climb a tree, wait for a fireman jump to fireman then scratch his face. Roll over and sun my belly. I vomit in the bed in the middle of the night lick left leg for ninety minutes, still dirty what the heck just happened, something feels fishy so eat from dog's food.</p>  
+        </div>
+      </Col>
+    </Row>
+  </Container>
+        
+)
--- src/components/ShopAll.js
+++ src/components/ShopAll.js
@@ -3,12 +3,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 
 
-export const ContactUs = () => (
+export const ShopAll = () => (
   <Container>
     <Row>
       <Col>
       <div>
-          <h2>Contact Us</h2>
+          <h2>Shop All</h2>
           <p>Cat ipsum dolor sit amet, chew foot get suspicious of own shadow then go play with toilette paper. Climb a tree, wait for a fireman jump to fireman then scratch his face. Roll over and sun my belly. I vomit in the bed in the middle of the night lick left leg for ninety minutes, still dirty what the heck just happened, something feels fishy so eat from dog's food.</p>  
         </div>
       </Col>
