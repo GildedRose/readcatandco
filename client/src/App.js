5259807bc7efd127a061e2e526b4cf336eb61077
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-26 13:09:38 -0500

    use apolloclient to bring server data to client

--- client/src/App.js
+++ client/src/App.js
@@ -12,26 +12,34 @@ import { NavigationBar } from './components/Nav/Nav.js';
 //import { Jumbotron } from './components/Jumbotron';
 /*import { Footer } from './components/Footer';*/
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-class App extends Component{
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
+class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <NavigationBar />
-        <Layout>
-          <Router>
-            <Switch>
-              {/* <Route exact path="/Home" component={Home} /> */}
-              <Route path="/ShopAll" component={ShopAll} />
-              <Route path="/About" component={About} />
-              <Route path="/ContactUs" component={ContactUs} />
-              <Route path="/LogIn" component={LogIn} />
-              <Route path="/CreateAccount" component={CreateAccount} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
+      <ApolloProvider client={client}>
+        <React.Fragment>
+          <NavigationBar />
+          <Layout>
+            <Router>
+              <Switch>
+                {/* <Route exact path="/Home" component={Home} /> */}
+                <Route path="/ShopAll" component={ShopAll} />
+                <Route path="/About" component={About} />
+                <Route path="/ContactUs" component={ContactUs} />
+                <Route path="/LogIn" component={LogIn} />
+                <Route path="/CreateAccount" component={CreateAccount} />
+                <Route component={NoMatch} />
+              </Switch>
+            </Router>
           </Layout>
-      </React.Fragment>
+        </React.Fragment>
+      </ApolloProvider>
     )
   }
 }
