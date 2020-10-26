7d20860db809cc1eea4548cabf4c228db1501a9d
Anne <anne.k.plummer@gmail.com>
2020-10-25 22:17:54 -0500

    working on Jumbotron and home layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { ShopAll } from './components/ShopAll';
+import { About } from './components/About';
+import { ContactUs } from './components/ContactUs';
+import { LogIn } from './components/LogIn';
+import { CreateAccount } from './components/CreateAccount';
+import { NoMatch } from './components/NoMatch';
+import { Layout } from './components/Layouts';
+import { NavigationBar } from './components/Nav/Nav.js';
+//import { Jumbotron } from './components/Jumbotron';
+/*import { Footer } from './components/Footer';*/
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          The Red Cat and Co
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/Home" component={Home} />
+              <Route path="/ShopAll" component={ShopAll} />
+              <Route path="/About" component={About} />
+              <Route path="/ContactUs" component={ContactUs} />
+              <Route path="/LogIn" component={LogIn} />
+              <Route path="/CreateAccount" component={CreateAccount} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+          </Layout>
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
