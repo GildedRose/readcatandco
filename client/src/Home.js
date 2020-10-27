882f7a476e633b8b09bc02fa6f572d532657daa0
Anne <anne.k.plummer@gmail.com>
2020-10-26 23:32:05 -0500

    Product Category boxes

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './Home.css'
 
+
+
 export const Home = () => (
+  <div>
   <div class="bus position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5" >
-      <h1 class="display-4 font-weight-normal">Punny headline</h1>
-        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
+      <h1 class="display-4 font-weight-bold">Welcome On Board!</h1>
           <a class="btn btn-outline-secondary" href="#">Coming soon</a>
       </div>
+  </div>
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+  <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Home Goods</h2>
     </div>
+  </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Clothes</h2>
+    </div>
+  </div>
+  <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+    <div class="my-3 py-3">
+      <h2 class="display-5">Pins</h2>
+    </div>
+  </div>
+  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="my-3 p-3">
+      <h2 class="display-5">Accessories</h2>
+    </div>
+  </div>
+</div>
+</div>
+    
 ) 
 
