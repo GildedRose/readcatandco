6f51167f50d1bdb6ec4c19b44e631a1aa19dd5fb
Devmadia <agathaphan@icloud.com>
2020-10-28 15:05:03 -0500

    updated about with initial state, added owner picture

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,8 +7,9 @@ export const About = () => (
       <Col>
       <div>
           <h2>About</h2>
-          <p>Cat ipsum dolor sit amet, chew foot get suspicious of own shadow then go play with toilette paper. Climb a tree, wait for a fireman jump to fireman then scratch his face. Roll over and sun my belly. I vomit in the bed in the middle of the night lick left leg for ninety minutes, still dirty what the heck just happened, something feels fishy so eat from dog's food.</p>  
-        </div>
+            <p>As a New York City transplant to San Antonio, Texas, one of the first things Jess noticed upon settling in was a distinct lack of locally made products availablefor sale in the area.</p>
+            <p>Accustomed to seeing vendors with goods available from local marketplaces and native products to the area, she aspired to bring a bit of home to South Texas.</p>
+      </div>
       </Col>
     </Row>
   </Container>
