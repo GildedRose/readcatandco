bff1e46cd446af082a1d134ac3b3828086ac308d
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-29 15:05:46 -0500

    finish backed for stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -61,6 +61,7 @@ const resolvers = {
             }
         },
         checkout: async (parent, args, context) => {
+            const url = new URL(context.headers.referer).origin;
             const order = new Order({ products: args.products });
             const { products } = await order.populate('products').execPopulate();
 
@@ -70,7 +71,8 @@ const resolvers = {
                 // generate product id
                 const product = await stripe.products.create({
                     name: products[i].name,
-                    description: products[i].description
+                    description: products[i].description,
+                    images: [`${url}/images/${products[i].image}`]
                 });
 
                 // generate price id using the product id
@@ -91,8 +93,8 @@ const resolvers = {
                 payment_method_types: ['card'],
                 line_items,
                 mode: 'payment',
-                success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url: 'https://example.com/cancel'
+                success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${url}/`
             });
 
             return { session: session.id };
