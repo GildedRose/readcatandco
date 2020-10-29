8c2d60107a736eca75e113a6194c11b92ef7954c
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-29 11:06:36 -0500

    comment out unused assets

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Auth from '../../utils/auth';
-import Cart from '../Cart';
+// import Cart from '../Cart';
 
 
 
