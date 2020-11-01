a3846f2a99216e092c54ca024ddcc2be6b6d5247
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-11-01 13:35:57 -0600

    style success page, add order

--- client/src/components/Pages/Success.js
+++ client/src/components/Pages/Success.js
@@ -5,11 +5,35 @@ import { idbPromise } from "../../utils/indexedDB";
 
 function Success() {
 
-    return(
-        <div>
-            <h1>Success!</h1>
-            <h2>Thank you so much for your purchase</h2>
-            <h2>You will now be redirected to the homepage</h2>
+    const [addOrder] = useMutation(ADD_ORDER);
+
+    useEffect(() => {
+        async function saveOrder() {
+            // const cart = await idbPromise('cart', 'get');
+            // const products = cart.map(item => item._id);
+
+            // if (products.length) {
+            //     const { data } = await addOrder({ variables: { products } });
+            //     const productData = data.addOrder.products;
+
+            //     productData.forEach((item) => {
+            //         idbPromise('cart', 'delete', item);
+            //     });
+            // }
+
+            setTimeout(() => {
+                window.location.assign('/');
+            }, 3000);
+        }
+
+        saveOrder();
+    }, [addOrder]);
+
+    return (
+        <div class="text-center">
+            <h1><i class="far fa-check-circle"></i></h1>
+            <h2>Thank you for your purchase</h2>
+            <p>You will now be redirected to the homepage...</p>
         </div>
     )
 };
